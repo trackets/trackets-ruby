c61150c81b4ab1f4ae50c0ab32b1525e435b7c6c
Jan Votava <votava@deployment.cz>
2014-05-20 12:58:27 +0200

    Add trackets-key to the the view helper

--- lib/trackets/view_helpers.rb
+++ lib/trackets/view_helpers.rb
@@ -1,7 +1,7 @@
 module Trackets
   module ViewHelpers
     def trackets_include_tag
-      javascript_include_tag "//trackets.s3.amazonaws.com/client.js"
+      javascript_include_tag "//trackets.s3.amazonaws.com/client.js", data: { "trackets-key" => Trackets.configuration.api_key }
     end
   end
 end
