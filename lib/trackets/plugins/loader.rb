44e7914cb388e0f0e0a1c16cbb9c05869327cc7e
Jan Votava <votava@deployment.cz>
2014-06-17 21:29:23 +0200

    Fix const_get for Ruby 2.0-

--- lib/trackets/plugins/loader.rb
+++ lib/trackets/plugins/loader.rb
@@ -7,7 +7,7 @@ module Trackets
           require "trackets/plugins/#{plugin_name}"
 
           class_name = plugin_name.to_s.split('_').map{|e| e.capitalize}.join
-          Object.const_get("Trackets::Plugins::#{class_name}").new
+          Trackets::Plugins.const_get(class_name).new
         end
       end
 
