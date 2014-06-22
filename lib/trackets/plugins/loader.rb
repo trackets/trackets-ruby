969881d35b507789921140e215c0feeb01695689
Jan Votava <votava@deployment.cz>
2014-06-22 12:31:37 +0200

    Initialize plugins only once

--- lib/trackets/plugins/loader.rb
+++ lib/trackets/plugins/loader.rb
@@ -4,10 +4,10 @@ module Trackets
 
       def initialize
         Trackets.configuration.load_plugins.each do |plugin_name|
-          require "trackets/plugins/#{plugin_name}"
-
-          class_name = plugin_name.to_s.split('_').map{|e| e.capitalize}.join
-          Trackets::Plugins.const_get(class_name).new
+          if require "trackets/plugins/#{plugin_name}"
+            class_name = plugin_name.to_s.split('_').map{|e| e.capitalize}.join
+            Trackets::Plugins.const_get(class_name).new
+          end
         end
       end
 
