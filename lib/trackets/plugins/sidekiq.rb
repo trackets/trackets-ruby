70dc152fc4d3cb354eeeadecce1fb8cbc817cb6a
Jan Votava <votava@deployment.cz>
2014-06-24 08:43:41 +0200

    Send info about Sidekiq context to server

--- lib/trackets/plugins/sidekiq.rb
+++ lib/trackets/plugins/sidekiq.rb
@@ -5,7 +5,7 @@ module Trackets
       def initialize
         if defined?(::Sidekiq)
           ::Sidekiq.configure_server do |config|
-            config.error_handlers << Proc.new {|ex,ctx_hash| Trackets.notify(ex) }
+            config.error_handlers << Proc.new {|ex,ctx_hash| Trackets.notify(ex, nil, sidekiq_context: ctx_hash) }
           end
         end
       end
